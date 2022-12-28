bea876dcb504e60f1f49b3492439873459c31acf
Nelson-iitp <nelson_2121cs07@iitp.ac.in>
2022-12-29 01:10:31 +0530

    begin

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 from setuptools import setup
-
 setup(
-    name =          'known',
-    version =       '0.0.5',  # 0.0.x is for unstable versions
-    url =           "https://github.com/NelsonSharma/Public",
-    author =        "Nelson.S",
-    author_email =  "mail.nelsonsharma@gmail.com",
-    description =   '~',
-    long_description="~",
-    long_description_content_type="text/markdown",
-    #py_modules =    [""],
-    packages =      ['known'],
-    license =       'Apache2.0',
-    package_dir =   { '' : 'src'},
-    install_requires = [],
-    include_package_data=True
+    name =                      'known',
+    version =                   '0.0.1',
+    url =                       'https://github.com/Nelson-iitp/known',
+    author =                    'Nelson.S',
+    author_email =              'nelson_2121cs07@iitp.ac.in',
+    description =               'known',
+    packages =                  ['known'],
+    classifiers=                ['License :: OSI Approved :: MIT License'],
+    package_dir =               { '' : 'modules'},
+    install_requires =          [],
+    include_package_data =      True
 )
