0a2e1baea99998a76cad5a6744f586288ff286ee
Nelson-iitp <nelson_2121cs07@iitp.ac.in>
2023-12-02 19:30:55 +0530

    version13

--- module/known/ktf/__init__.py
+++ module/known/ktf/__init__.py
@@ -2,7 +2,7 @@
 from .basic import *
 from .extra import *
 from .opt import *
-from .attention import *
+#from .attention import *
 from .torchtf import CODER_DEFAULT_ARGS, Encoder, Decoder
 from .tf import *
 #from .trainer import *
